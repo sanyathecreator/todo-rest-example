ed3fd049b24c07892373cf1efdcbe73a682a4d8f
sanyathecreator <olexxxandr.romaniuk@gmail.com>
2026-05-06 21:52:02 +0200

    change model for db management

--- internal/models/task.go
+++ internal/models/task.go
@@ -3,11 +3,12 @@ package models
 import "time"
 
 type Task struct {
-	Title       string     `json:"title"`
-	Description string     `json:"description"`
-	Completed   bool       `json:"completed"`
-	CreatedAt   time.Time  `json:"created_at"`
-	CompletedAt *time.Time `json:"completed_at"`
+	ID          int        `json:"id" db:"id"`
+	Title       string     `json:"title" db:"title"`
+	Description string     `json:"description" db:"description"`
+	Completed   bool       `json:"completed" db:"completed"`
+	CreatedAt   time.Time  `json:"created_at" db:"created_at"`
+	CompletedAt *time.Time `json:"completed_at" db:"updated_at"`
 }
 
 type TaskDTO struct {
@@ -31,6 +32,7 @@ func NewTask(title, description string) Task {
 	}
 }
 
+// BUG: panics when CompletedAt is nil (which it always is for a new task) *t.CompletedAt = time.Now()
 func (t *Task) ToggleCompletion() {
 	if !t.Completed {
 		*t.CompletedAt = time.Now()
