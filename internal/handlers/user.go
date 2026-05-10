8a867214633667171dfbcb17308cceb19221a598
sanyathecreator <olexxxandr.romaniuk@gmail.com>
2026-05-10 13:05:23 +0200

    add generating JWT token and login user functionality

--- internal/handlers/user.go
+++ internal/handlers/user.go
@@ -5,7 +5,9 @@ import (
 	"net/http"
 	"sanyathecreator/todo-rest-example/internal/models"
 	"strings"
+	"time"
 
+	"github.com/golang-jwt/jwt/v5"
 	"golang.org/x/crypto/bcrypt"
 )
 
@@ -37,6 +39,44 @@ func (h *Handlers) RegisterUser(w http.ResponseWriter, r *http.Request) {
 	}
 
 	respondWithJSON(w, http.StatusCreated, user)
+}
+
+func (h *Handlers) LoginUser(w http.ResponseWriter, r *http.Request) {
+	var dto models.UserDTO
+
+	if err := json.NewDecoder(r.Body).Decode(&dto); err != nil {
+		respondWithError(w, http.StatusBadRequest, "set incorrect data")
+		return
+	}
+
+	user, err := h.repo.GetUserByEmail(r.Context(), dto.Email)
+	if err != nil {
+		respondWithError(w, http.StatusUnauthorized, "invalid credentials")
+		return
+	}
+
+	if err := bcrypt.CompareHashAndPassword([]byte(user.PasswordHash), []byte(dto.Password)); err != nil {
+		respondWithError(w, http.StatusUnauthorized, "invalid credentials")
+		return
+	}
+
+	token, err := generateToken(user.ID)
+	if err != nil {
+		respondWithError(w, http.StatusInternalServerError, "something went wrong")
+		return
+	}
+
+	respondWithJSON(w, http.StatusOK, map[string]string{"token": token})
+}
+
+func generateToken(userID int) (string, error) {
+	claims := jwt.MapClaims{
+		"user_id": userID,
+		"exp":     time.Now().Add(24 * time.Hour).Unix(),
+	}
+
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+	return token.SignedString([]byte("verysecret"))
 }
 
 func hashPassword(password string) (string, error) {
